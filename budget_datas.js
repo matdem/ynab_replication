d0e6c489cdd440ec6a20d4ee046c10686d666696
matdem <math.demers@protonmail.com>
2020-05-21 22:34:36 -0400

    Separate datas from the js file and put it in "./budget_datas.js". Add the datas file in the html <head>

---
+++ budget_datas.js
@@ -0,0 +1,44 @@
+// Budget's Datas
+var budget = {
+  year: 2019,
+  categories: [
+    {
+      name: "Obligations",
+      totalBudgeted: 0,
+      totalAvailable: 0,
+    },
+    {
+      name: "Voitures",
+      totalBudgeted: 0,
+      totalAvailable: 0,
+    },
+  ],
+  costItems: [
+    {
+      name: "Hypothèque",
+      category: "Obligations",
+      budgeted: 100,
+      available: 150,
+    },
+    {
+      name: "Sogetel",
+      category: "Obligations",
+      budgeted: 116,
+      available: 0,
+    },
+    {
+      name: "Essence",
+      category: "Voitures",
+      budgeted: 50,
+      available: 30,
+    },
+    {
+      name: "Entretien",
+      category: "Voitures",
+      budgeted: 100,
+      available: 70,
+    },
+  ],
+};
+
+// export { budget };
