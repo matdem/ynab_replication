f807d225a525cc87f7b0eaa9a76ddd8ccdddbdb9
matdem <math.demers@protonmail.com>
2020-05-21 22:37:36 -0400

    Refactoring the entire code of ynab.js

--- ynab.js
+++ ynab.js
@@ -1,81 +1,33 @@
-// Budget's Datas
-var budget = {
-  year: 2019,
-  categories: [
-    {
-      name: "Obligations",
-      budgeted: 0,
-      available: 0
-    },
-    {
-      name: "Voitures",
-      budgeted: 0,
-      available: 0
-    }
-  ],
-  costItems: [
-    {
-      name: "Hypothèque",
-      category: "Obligations",
-      budgeted: 100,
-      available: 150
-    },
-    {
-      name: "Sogetel",
-      category: "Obligations",
-      budgeted: 116,
-      available: 0
-    },
-    {
-      name: "Essence",
-      category: "Voitures",
-      budgeted: 50,
-      available: 30
-    },
-    {
-      name: "Entretien",
-      category: "Voitures",
-      budgeted: 100,
-      available: 70
-    }
-  ]
-};
-
-for (let category of budget.categories) {
-  for (let item of budget.costItems) {
-    if (category.name === item.category) {
-      category.budgeted += item.budgeted;
-      category.available += item.available;
-    }
-  }
-}
-
-// Creation of a table with budget's datas
-var budgetElt = document.getElementById("budget");
+// Creation of a table with budget's datas from "./budget_datas.js"
+const budgetElt = document.getElementById("budget");
 
 // Categories rows
-for (let i = 0; i < budget.categories.length; i++) {
-  let category = budget.categories[i];
+for (let category of budget.categories) {
+  // Calculate total budgeted and total available
+  for (let item of budget.costItems) {
+    if (category.name === item.category) {
+      category.totalBudgeted += item.budgeted;
+      category.totalAvailable += item.available;
+    }
+  }
 
-  let div = document.createElement("div");
+  // Create a div for each category and set its attributes
+  const div = document.createElement("div");
   div.className = "category";
   div.id = category.name;
 
-  var p = document.createElement("p");
-
   // Add each head colum's category
-  for (let a = 0; a < Object.values(category).length; a++) {
-    var p = document.createElement("p");
-    var value = Object.keys(category)[a];
-    switch (value) {
+  for (let property in category) {
+    const p = document.createElement("p");
+    switch (property) {
       case "name":
         p.textContent = category.name;
         break;
-      case "budgeted":
-        p.innerHTML = `Budgeted<br />${category.budgeted}$`;
+      case "totalBudgeted": // Add the head title and the total
+        p.innerHTML = `Budgeted<br />${category.totalBudgeted}$`;
         break;
-      case "available":
-        p.innerHTML = `Available<br />${category.available}$`;
+      case "totalAvailable": // Add the head title and the total
+        p.innerHTML = `Available<br />${category.totalAvailable}$`;
         break;
     }
     div.appendChild(p);
@@ -84,28 +36,30 @@ for (let i = 0; i < budget.categories.length; i++) {
 }
 
 // Cost items rows
-for (let i = 0; i < budget.costItems.length; i++) {
-  var costItem = budget.costItems[i];
-
-  var div = document.createElement("div");
+for (let costItem of budget.costItems) {
+  // Create a div for each costItem with its attributes
+  const div = document.createElement("div");
   div.className = "costItem";
   div.classList.add(costItem.category);
 
-  for (var a = 0; a < Object.values(costItem).length; a++) {
+  // Add the content of each row
+  for (let a = 0; a < Object.values(costItem).length; a++) {
+    // Discard the "category" property because it's not in the layout
     if (Object.keys(costItem)[a] !== "category") {
-      var p = document.createElement("p");
-
+      const p = document.createElement("p");
       // Add "$" for numbers
       if (typeof Object.values(costItem)[a] === "number") {
         p.textContent = Object.values(costItem)[a] + "$";
       } else {
         p.textContent = Object.values(costItem)[a];
       }
+      div.appendChild(p);
     }
-    div.appendChild(p);
   }
+
+  // Classify each row with the good category
   for (let category of budget.categories) {
-    var divCategory = document.getElementById(category.name);
+    const divCategory = document.getElementById(category.name);
     if (div.classList.contains(category.name)) {
       budgetElt.insertBefore(div, divCategory.nextSibling);
     }
