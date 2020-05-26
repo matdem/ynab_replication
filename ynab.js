ed7cab23aa5ebf88cc59fa6a2bb00c7a37141bff
matdem <math.demers@protonmail.com>
2020-05-25 21:46:05 -0400

    Rename variables that refer to a DOM element

--- ynab.js
+++ ynab.js
@@ -12,56 +12,56 @@ for (let category of budget.categories) {
   }
 
   // Create a div for each category and set its attributes
-  const div = document.createElement("div");
-  div.className = "category";
-  div.id = category.name;
+  const divElt = document.createElement("div");
+  divElt.className = "category";
+  divElt.id = category.name;
 
   // Add each head colum's category
   for (let property in category) {
-    const p = document.createElement("p");
+    const pElt = document.createElement("p");
     switch (property) {
       case "name":
-        p.textContent = category.name;
+        pElt.textContent = category.name;
         break;
       case "budgeted": // Add the head title and the total
-        p.innerHTML = `Budgeted<br />${category.budgeted}$`;
+        pElt.innerHTML = `Budgeted<br />${category.budgeted}$`;
         break;
       case "available": // Add the head title and the total
-        p.innerHTML = `Available<br />${category.available}$`;
+        pElt.innerHTML = `Available<br />${category.available}$`;
         break;
     }
-    div.appendChild(p);
+    divElt.appendChild(pElt);
   }
-  budgetElt.appendChild(div);
+  budgetElt.appendChild(divElt);
 }
 
 // Cost items rows
 for (let costItem of budget.costItems) {
   // Create a div for each costItem and set its attributes
-  const div = document.createElement("div");
-  div.className = "costItem";
-  div.classList.add(costItem.category);
+  const divElt = document.createElement("div");
+  divElt.className = "costItem";
+  divElt.classList.add(costItem.category);
 
   // Add the content of each row
   for (let property in costItem) {
     // Discard the "category" property because it's not in the layout
     if (property !== "category") {
-      const p = document.createElement("p");
+      const pElt = document.createElement("p");
       // Add "$" for numbers
       if (typeof costItem[property] === "number") {
-        p.textContent = costItem[property] + "$";
+        pElt.textContent = costItem[property] + "$";
       } else {
-        p.textContent = costItem[property];
+        pElt.textContent = costItem[property];
       }
-      div.appendChild(p);
+      divElt.appendChild(pElt);
     }
   }
 
   // Classify each row with the good category
   for (let category of budget.categories) {
     const divCategory = document.getElementById(category.name);
-    if (div.classList.contains(category.name)) {
-      budgetElt.insertBefore(div, divCategory.nextSibling);
+    if (divElt.classList.contains(category.name)) {
+      budgetElt.insertBefore(divElt, divCategory.nextSibling);
     }
   }
 }
